5ff7b693cd9ec2ab888f0cb6ba92282f49043fe9
Molotok23 <popkinv774@gmail.com>
2026-03-11 23:50:20 +0500

    Подготовил для Render

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -145,3 +146,7 @@ async def get_balance(username: str):
 # Запуск сервера (для теста)
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
